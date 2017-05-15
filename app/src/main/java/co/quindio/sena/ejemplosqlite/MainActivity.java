6ab2300c7e8899bb71bdd142281816d73b6fbb1f
chenaoh <cdhenao@gmail.com>
2017-05-14 22:14:26 -0500

    otro cambio - chenao

--- app/src/main/java/co/quindio/sena/ejemplosqlite/MainActivity.java
+++ app/src/main/java/co/quindio/sena/ejemplosqlite/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        //otro comentario
         Intent miIntent=null;
         switch (view.getId()){
             case R.id.btnOpcionRegistro:
