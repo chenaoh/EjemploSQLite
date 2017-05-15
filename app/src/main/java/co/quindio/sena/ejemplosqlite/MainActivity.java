27e766fde05a5c7855dbe076f95c2eebbb03b19d
chenaoh <cdhenao@gmail.com>
2017-05-14 19:49:34 -0500

    Initial commit

---
+++ app/src/main/java/co/quindio/sena/ejemplosqlite/MainActivity.java
@@ -0,0 +1,36 @@
+package co.quindio.sena.ejemplosqlite;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_usuarios",null,1);
+
+
+
+    }
+
+    public void onClick(View view) {
+        Intent miIntent=null;
+        switch (view.getId()){
+            case R.id.btnOpcionRegistro:
+                miIntent=new Intent(MainActivity.this,RegistroUsuariosActivity.class);
+                break;
+        }
+        if (miIntent!=null){
+            startActivity(miIntent);
+        }
+
+    }
+}
