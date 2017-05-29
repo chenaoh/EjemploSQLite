e43142f9d1b1c049fe0c4e90904fb0eab0292d15
chenaoh <cdhenao@gmail.com>
2017-05-29 14:51:20 -0500

    otro cambio - chenao otro cambio otro 7

--- app/src/main/java/co/quindio/sena/ejemplosqlite/MainActivity.java
+++ app/src/main/java/co/quindio/sena/ejemplosqlite/MainActivity.java
@@ -16,14 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_usuarios",null,1);
-        //prueba 2
-        //prueba 1
-        //Otra prueba subiendo info
-        //prueba 3
-        //prueba 5
-        //prueba 4
-        //prueba 6
-        //prueba 7
     }
 
     public void onClick(View view) {
